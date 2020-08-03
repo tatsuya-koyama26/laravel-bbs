aa19b97fb735a78f9d59ec654a7b5d8fbb503c4a
tatsuya-koyam26 <koyama.tatsuya@icloud.com>
2020-08-03 16:28:45 +0900

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostsController extends Controller{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        Post::create($params);
+
+        return redirect()->route('top');
+    }
+
+    public function show($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        return view('posts.show', ['post' => $post]);
+    }
+
+    public function edit($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update($post_id, Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        $post = Post::findOrFail($post_id);
+        $post->fill($params)->save();
+
+        return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function destroy($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        DB::transaction(function () use ($post) {
+            $post->comments()->delete();
+            $post->delete();
+        });
+
+        return redirect()->route('top');
+    }
+}
